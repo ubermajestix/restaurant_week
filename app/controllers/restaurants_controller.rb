4f22bccfd449354365760956d5a9d93f5b8bb89b
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2010-02-23 13:59:43 -0700

    fix for pg

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all(:conditions=>["lat not null and lng not null"])
+    @restaurants = Restaurant.all(:conditions=>["lat != NULL and lng != NULL"])
   end
   
 end
